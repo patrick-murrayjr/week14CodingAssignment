034f978e63b586c3f1700589f8183347fe4fdd44
Patrick Murray <patrick.murrayjr@gmail.com>
2023-07-08 00:12:52 -0400

    added data to data.jsx module

---
+++ src/assets/data/data.jsx
@@ -0,0 +1,194 @@
+let imgAvengers;
+let imgInception;
+let imgInterstellar;
+let imgDarkKnight;
+export const movieData = [
+   {
+      id: 1,
+      title: 'The Avengers',
+      image: imgAvengers,
+      director: 'Joss Whedon',
+      writer: 'Joss Whedon',
+      starring: 'Robert Downey Jr., Chris Evans, Mark Ruffalo',
+      synopsis:
+         "Earth's mightiest heroes must come together and learn to fight as a team.",
+      rating: 'PG-13',
+   },
+   {
+      id: 2,
+      title: 'Inception',
+      image: imgInception,
+      director: 'Christopher Nolan',
+      writer: 'Christopher Nolan',
+      starring: 'Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page',
+      synopsis: 'A thief who steals corporate secrets by entering the subconscious.',
+      rating: 'PG-13',
+   },
+   {
+      id: 3,
+      title: 'Interstellar',
+      image: imgInterstellar,
+      director: 'Christopher Nolan',
+      writer: 'Jonathan Nolan, Christopher Nolan',
+      starring: 'Matthew McConaughey, Anne Hathaway, Jessica Chastain',
+      synopsis:
+         "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+      rating: 'PG-13',
+   },
+   {
+      id: 4,
+      title: 'The Dark Knight',
+      image: imgDarkKnight,
+      director: 'Christopher Nolan',
+      writer: 'Jonathan Nolan, Christopher Nolan',
+      starring: 'Christian Bale, Heath Ledger, Aaron Eckhart',
+      synopsis: 'Batman faces off against the Joker, a villain who thrives on chaos.',
+      rating: 'PG-13',
+   },
+];
+
+export const reviewData = [
+   {
+      movieID: 1,
+      id: 1,
+      author: 'John Doe',
+      stars: 5,
+      review: 'Great movie! Amazing action sequences and a compelling story.',
+   },
+   {
+      movieID: 1,
+      id: 2,
+      author: 'Jane Smith',
+      stars: 3,
+      review: 'Good movie, but could have been better. Some scenes felt unnecessary.',
+   },
+   {
+      movieID: 1,
+      id: 3,
+      author: 'Mike Johnson',
+      stars: 4,
+      review:
+         'A thrilling superhero ensemble! The chemistry between the characters is fantastic, and the visual effects are mind-blowing.',
+   },
+   {
+      movieID: 1,
+      id: 4,
+      author: 'Sarah Lee',
+      stars: 5,
+      review:
+         'As a Marvel fan, I loved every moment of "The Avengers"! It is a brilliant combination of action, humor, and heartwarming moments.',
+   },
+   {
+      movieID: 1,
+      id: 5,
+      author: 'David Williams',
+      stars: 4,
+      review:
+         'A rollercoaster of emotions! The movie delivers on the promise of an epic team-up and leaves you wanting more.',
+   },
+   {
+      movieID: 1,
+      id: 6,
+      author: 'Emily Adams',
+      stars: 3,
+      review:
+         'Decent movie, but some characters deserved more screen time. The focus seemed a bit uneven.',
+   },
+   {
+      movieID: 1,
+      id: 7,
+      author: 'Chris Anderson',
+      stars: 5,
+      review:
+         'An unforgettable cinematic experience! "The Avengers" is a milestone in superhero filmmaking.',
+   },
+   {
+      movieID: 2,
+      id: 8,
+      author: 'Alice Johnson',
+      stars: 5,
+      review:
+         "One of the best movies I've ever seen. Mind-bending and thought-provoking.",
+   },
+   {
+      movieID: 2,
+      id: 9,
+      author: 'Bob Anderson',
+      stars: 4,
+      review: 'A masterpiece of storytelling. The visuals are stunning.',
+   },
+   {
+      movieID: 2,
+      id: 10,
+      author: 'Chris Wilson',
+      stars: 3,
+      review: "Good movie, but it's a bit confusing at times.",
+   },
+   {
+      movieID: 2,
+      id: 11,
+      author: 'Sarah Thompson',
+      stars: 3,
+      review: 'I was totally lost. The movie is too complicated for me.',
+   },
+   {
+      movieID: 3,
+      id: 12,
+      author: 'David Smith',
+      stars: 4,
+      review: 'A visually stunning and emotionally powerful film. Highly recommend!',
+   },
+   {
+      movieID: 3,
+      id: 13,
+      author: 'Emily Davis',
+      stars: 5,
+      review: "One of the best sci-fi movies I've ever seen. It's a masterpiece.",
+   },
+   {
+      movieID: 3,
+      id: 14,
+      author: 'Michael Johnson',
+      stars: 4,
+      review: 'Great movie with exceptional performances and a captivating story.',
+   },
+   {
+      movieID: 3,
+      id: 15,
+      author: 'Sarah Thompson',
+      stars: 4,
+      review:
+         "Interstellar takes you on an incredible journey and leaves you thinking about it long after it's over.",
+   },
+   {
+      movieID: 4,
+      id: 16,
+      author: 'John Williams',
+      stars: 5,
+      review:
+         "An absolute masterpiece. Heath Ledger's performance as the Joker is outstanding.",
+   },
+   {
+      movieID: 4,
+      id: 17,
+      author: 'Rebecca Johnson',
+      stars: 4,
+      review:
+         'Dark and gritty with intense action sequences. One of the best superhero movies ever made.',
+   },
+   {
+      movieID: 4,
+      id: 18,
+      author: 'James Anderson',
+      stars: 4,
+      review:
+         "A thrilling and dark movie with exceptional performances. Heath Ledger's portrayal of the Joker is unforgettable.",
+   },
+   {
+      movieID: 4,
+      id: 19,
+      author: 'Jill Harris',
+      stars: 3,
+      review: 'It was too dark. I prfer the classic Batman movies from the 90s.',
+   },
+];
